9cda047b113d003a8aa3bda52da363507dcd6c27
Bryan Muñoz <swsbmm@gmail.com>
2023-10-24 11:45:21 -0500

    FIX: refactor the way you get he db session

--- back/app/database/database.py
+++ back/app/database/database.py
@@ -1,8 +1,20 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, scoped_session
 from core.config import DATABASE_URL
 
+# Configuracion de sqlalchemy y conexion a la base de datos mysql
+
 engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
 Base = declarative_base()
+
+
+class Database:
+    def __init__(self):
+        self.session = SessionLocal()
+    def __enter__(self):
+        return self.session
+    def __exit__(self, exc_type, exc_value, traceback):
+        print("CONEXION CERRADA")
+        self.session.close()
