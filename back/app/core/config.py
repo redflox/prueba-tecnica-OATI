0183ee5c6d9689fd474a880ab2c7ca21f9522afa
Bryan Muñoz <swsbmm@gmail.com>
2023-10-24 11:44:08 -0500

    FEAT: separate the configuration

--- back/app/core/config.py
+++ back/app/core/config.py
@@ -3,5 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv(".env")
 
+# Configuracion global del proyecto a partir de un archivo .env a  la altura del fichero main.py
 
 DATABASE_URL = os.getenv("DATABASE_URL")
